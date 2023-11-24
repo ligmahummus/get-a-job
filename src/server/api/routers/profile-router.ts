332d4aa42ce8b2199fee4f80c241cdda63794f4e
ligmahummus <xarielah@gmail.com>
2023-11-24 15:41:57 +0200

    feat: dynamic profile pages

--- src/server/api/routers/profile-router.ts
+++ src/server/api/routers/profile-router.ts
@@ -4,14 +4,10 @@ import * as z from "zod";
 const profileRouter = createTRPCRouter({
   getProfileBySlug: publicProcedure
     .input(z.string())
-    .query(async ({ ctx, input }) => {
-      const user = await ctx.db.profile.findFirst({
+    .query(({ ctx, input }) => {
+      return ctx.db.profile.findFirst({
         where: { slug: input, active: true },
       });
-
-      console.log(user);
-
-      return user;
     }),
 });
 
